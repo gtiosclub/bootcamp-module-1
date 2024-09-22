64868657ec26a76d558772f8931eb0775bf25311
esanint <esanint05@gmail.com>
2024-09-22 16:47:13 -0400

    Completed Challenge 1,2 of Conditionals

--- SwiftBasics.playground/Pages/Conditionals.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Conditionals.xcplaygroundpage/Contents.swift
@@ -13,12 +13,33 @@ import Foundation
 // CHALLENGE 1: Gary has a smart alarm system which reads a message (alarmMessage) depending on whether he's awake (represented by isAwake). If Gary is awake, set the alarm message to "Good morning Gary!", otherwise set it to "Wake up Gary!". Recreate such a conditional check using if-else, ternary statements, and a guard statement (inside the else block, instead of `return` perform `fatalError()`).
 let isAwake: Bool = true
 var alarmMessage: String
-// [start here]
+if isAwake {
+    alarmMessage = "Good morning Gary!"
+} else {
+    guard isAwake else {
+        alarmMessage = "Wake up Gary!"
+        fatalError()
+    }
+}
+
 
 // CHALLENGE 2: Using if-(else if)-else statements, perform the following condition check: if both gary (garyIsAwake) and max (maxIsAwake) are awake, print "How are you both feeling?". If only one of them is awake, print "How are you feeling X?", where X is the person awake, and "Wake up Y!" where Y is the other person (not awake). If none are awake, print "Wake up!".
 let garyIsAwake: Bool = true
 let maxIsAwake: Bool = false
+if garyIsAwake && maxIsAwake {
+    print("How are you both feeling?")
+} else if garyIsAwake && !maxIsAwake {
+    print("How are you feeling, Gary?")
+    print("Wake up Max!")
+} else if !garyIsAwake && maxIsAwake {
+    print("How are you feeling, Max?")
+    print("Wake up Gary!")
+} else {
+    print("Wake up!")
+}
+
 
 // CHALLENGE 3: test the combined used of &&, !, ||, <, >, <=, >= to achieve a conditional assignment. You are trying to bake a cake, you need 'flourNeeded' grams of flour to bake it. You currently have 'flourLeft' grams of flour. Write a conditional statement which prints "I have enough flour." if the amount of flour needed is available. Otherwise, print "I'll go buy more flour!" and increment the amount of flour you now have by 15.
 let flourLeft: Int = 10
 let flourNeeded: Int = 5
+
